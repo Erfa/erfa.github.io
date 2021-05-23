af36eb1c074111b4152c1eb1b1087dfe065ed2fb
Erik Fahlén <erfa90@gmail.com>
2021-05-23 19:01:41 +0200

    Replace React page with a beautiful portrait.

--- src/components/ReactIntro.jsx
+++ src/components/ReactIntro.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react'
-import logo from '../logo.svg'
+import portrait from '../portrait.png'
 
 export const ReactIntro = () => {
   useEffect(() => {
-    document.title = 'React App'
+    document.title = 'Erik Fahlén'
     document.getElementById('favicon').href = 'favicon.ico'
     document.getElementById('appleTouchIcon').href = 'logo192.png'
   })
 
   return <>
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <img src={portrait} className="Portrait" alt="portrait" />
+      <h1 className="App-title">Erik Fahlén</h1>
     </header>
-    <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+    <p className="App-intro">A very skilled developer.</p>
   </>
 }
